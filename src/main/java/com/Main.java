e8efabccb77862c5499025e153ae4daa3720796b
Zorbiks <176812563+Zorbiks@users.noreply.github.com>
2026-03-26 13:51:56 +0100

    Amend

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,6 +2,7 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Github!");
+        System.out.println("Second line");
     }
 }
